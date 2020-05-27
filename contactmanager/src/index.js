6bf41d88b89787f7205c7913f36909bdee7f84eb
chinmay <chinmay11.moghe@gmail.com>
2020-05-28 00:56:15 +0530

    Created an index.js file - kickstart

--- contactmanager/src/index.js
+++ contactmanager/src/index.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const App = ()=> {
+    return (
+        <div>Starting an app from scratch !!!</div>
+    );
+};
+
+ReactDOM.render(<App/>,document.querySelector("#root"));
