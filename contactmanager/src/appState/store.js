8ae9866726ec782f78034ef0676a8d40ba742d62
chinmay <chinmay11.moghe@gmail.com>
2020-06-01 21:55:46 +0530

    Added comment for store

--- contactmanager/src/appState/store.js
+++ contactmanager/src/appState/store.js
@@ -1,12 +1,12 @@
 // Used to create a store for app
-import {createStore } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducer';
-
+/*Store must be a single object, do not assign list item to it directly*/
 const initialState = {
-    contactList : ["Elon Musk", "Chris Coyer", "Keanu Reeves", "Sandra Bullock"]
+    contactList: ["Elon Musk", "Chris Coyer", "Keanu Reeves", "Sandra Bullock"]
 };
 
 function configureStore(state = initialState) {
-    return createStore(reducer,state);
+    return createStore(reducer, state);
 }
 export default configureStore;
